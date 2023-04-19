d0b67b100b2aa53bc16a4490a5734b20d09850c3
Tadas <vtadas25@gmail.com>
2023-04-19 12:36:59 +0300

    Remove generated js files

--- src/exceptions/InvalidArgument.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class InvalidArgument extends Error {
-}
-exports.default = InvalidArgument;
-;
-//# sourceMappingURL=InvalidArgument.js.map
--- src/exceptions/OpNotSupported.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class OpNotSupprtedError extends Error {
-}
-exports.default = OpNotSupprtedError;
-;
-//# sourceMappingURL=OpNotSupported.js.map
